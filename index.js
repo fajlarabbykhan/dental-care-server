01d16630292305a3a41f131d834315c5debbbfa0
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2022-12-07 13:52:32 +0600

    service creted

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.heyzvvw.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("db connected");
+
+    const serviceCollection = client.db("dental_care").collection("services");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Dental care listening on port ${port}`);
+});
