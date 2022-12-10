7f7be70809db97746d4685ed417d691115674508
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2022-12-10 16:01:22 +0600

    getting data for a user

--- index.js
+++ index.js
@@ -52,6 +52,14 @@ async function run() {
       res.send(services);
     });
 
+    app.get("/booking", async (req, res) => {
+      const patient = req.query.patient;
+      const query = { patient: patient };
+      const bookings = await bookingCollection.find(query).toArray();
+      // console.log(bookings);
+      res.send(bookings);
+    });
+
     app.post("/booking", async (req, res) => {
       const booking = req.body;
       const query = {
